4be75c9e4775feb0c4ce9223d95acf7696cdd11d
franclobo <fjbl2788@gmail.com>
2024-01-05 12:23:41 -0500

    Generate controller and views for searches and articles

---
+++ test/controllers/searches_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class SearchesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get searches_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get searches_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get searches_new_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get searches_edit_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get searches_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get searches_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get searches_destroy_url
+    assert_response :success
+  end
+end
